9b80d0d4b969aab06305a456d34cb8395c4e773b
Dyno Fu <dyno.fu@gmail.com>
2020-02-08 23:01:14 -0800

    15.7. Accessing Scalatra Web Service GET Parameters

--- scalatra-webapp/src/main/scala/me/dynofu/webapp/MyScalatraServlet.scala
+++ scalatra-webapp/src/main/scala/me/dynofu/webapp/MyScalatraServlet.scala
@@ -15,4 +15,32 @@ class MyScalatraServlet extends ScalatraServlet {
   get("/hello") {
     <p>Hello, world!</p>
   }
+
+  /**
+   * The URL
+   * http://localhost:8080/saveName?fname=Alvin&lname=Alexander
+   * prints: Some(Alvin), Some(Alexander)
+   */
+  get("/saveName") {
+    val firstName = params.get("fname")
+    val lastName = params.get("lname")
+
+    <p>{firstName}, {lastName}</p>
+  }
+
+  get("/hello/:fname/:lname") {
+    <p>Hello, {params("fname")}, {params("lname")}</p>
+  }
+
+  /**
+   * (1) GET http://localhost:8080/getFilename/Users/Al/tmp/file.txt
+   */
+  get("/getFilename/*.*") {
+    // (2) creates a Vector(Users/Al/tmp/file, txt)
+    val data = multiParams("splat")
+
+    // (3) prints: [Users/Al/tmp/file, txt]
+    <pre>{data.mkString("[", ", ", "]")}</pre>
+  }
+
 }
